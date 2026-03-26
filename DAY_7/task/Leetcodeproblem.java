48d9c4d314cb20d9f1b4349b3ffa2544c6703d25
Tamcodes4 <tamilselviannamalai24@gmail.com>
2026-03-26 11:00:20 +0530

    Updated DAY_7 task

--- DAY_7/task/Leetcodeproblem.java
+++ DAY_7/task/Leetcodeproblem.java
@@ -2,6 +2,6 @@ package DAY_7.task;
 
 public class Leetcodeproblem {
     public static void main(String[] args) {
-        System.out.println("yup");
+        System.out.println("hello");
     }
 }
