07c7f40e1a59321e713e2124b24feee1123dd256
Tamcodes4 <tamilselviannamalai24@gmail.com>
2026-03-26 11:07:01 +0530

    Updated DAY_7 problems_solved

--- DAY_7/problems_solved/Inheritance.java
+++ DAY_7/problems_solved/Inheritance.java
@@ -3,15 +3,22 @@ class Animal{
         System.out.println("has multiple species");
     }
 }
-class Dog{
+class Dog extends Animal {
     void bark(){
         System.out.print("Dog barks");
     }
 }
+class Puppy extends Dog{
+    void weeps(){
+System.out.println("Dog weeps");
+    }
+}
 public class Inheritance{
     public static void main(String[] args){
-        Dog sound = new Dog();
+        Puppy sound = new Puppy();
         sound.bark();
+        sound.display();
+        sound.weeps();
         System.out.println("Dog Barks"+sound.hashCode());
     }
 }
