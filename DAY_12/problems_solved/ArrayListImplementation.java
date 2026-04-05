09e944bc118b708077001c61bf11d0c0e6e425dd
Tamcodes4 <tamilselviannamalai24@gmail.com>
2026-04-05 22:43:39 +0530

    updated

---
+++ DAY_12/problems_solved/ArrayListImplementation.java
@@ -0,0 +1,12 @@
+package DAY_12.problems_solved;
+import java.util.*;
+import java.util.ArrayList;
+
+public class ArrayListImplementation {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(null);
+        list.add(10);
+        
+    }
+}
